2b61fb3ff4798e3272bf79513f3301bcc4d4ca19
ysdxz207 <ysdxz207@qq.com>
2018-01-28 02:37:30 +0800

    :simle:

--- source/jquery-jf.js
+++ source/jquery-jf.js
@@ -6,11 +6,15 @@
         fontSize: '14',
         lineHeight: '24',
         collapsed: false,
-        withQuotes: false
+        withQuotes: false,
+        showImg: true
     };
     var jf = {
         show: function (e1) {
             e1.fadeIn(200);
+        },
+        hide: function (e1) {
+            e1.hide();
         },
         collapse: function(el) {
 
@@ -34,7 +38,6 @@
         var html = '';
 
         if (typeof json === 'string') {
-            /* Escape tags */
             json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
             if (isUrl(json))
                 html += '<a href="' + json + '" class="json-value json-link" target="_blank">' + json + '</a>';
@@ -55,12 +58,10 @@
                 html += '[<ol class="json-value json-array">';
                 for (var i = 0; i < json.length; ++i) {
                     html += '<li>';
-                    /* Add toggle button if item is collapsable */
                     if (isCollapsable(json[i])) {
                         html += '<a href class="json-toggle"></a>';
                     }
                     html += json2html(json[i], options);
-                    /* Add comma if item is not last */
                     if (i < json.length - 1) {
                         html += ',';
                     }
@@ -80,16 +81,14 @@
                     if (json.hasOwnProperty(key)) {
                         html += '<li>';
                         var keyRepr = options.withQuotes ?
-                            '<span class="json-key">"' + key + '"</span>' : key;
-                        /* Add toggle button if item is collapsable */
+                            '<span class="json-key">"' + key + '"</span>' : '<span class="json-key">' + key + '</span>';
                         if (isCollapsable(json[key])) {
-                            html += '<a href class="json-toggle">' + keyRepr + '</a>';
+                            html += '<i class="json-toggle-btn fa fa-caret-down"><hr></i><span class="json-key">' + keyRepr + '</span>';
                         }
                         else {
                             html += keyRepr;
                         }
                         html += ': ' + json2html(json[key], options);
-                        /* Add comma if item is not last */
                         if (--key_count > 0)
                             html += ',';
                         html += '</li>';
@@ -110,53 +109,14 @@
         var json = arguments[0],
             options = arguments[1] || {};
         $.fn.extend(defaultOptions, options);
-        _this.html('<a href class="json-root json-toggle">JSON</a>')
 
-        return this.each(function() {
-            var html = json2html(json, defaultOptions);
-            _this.css('font-size', defaultOptions.fontSize + 'px');
-            _this.css('line-height', defaultOptions.lineHeight + 'px');
-            _this.addClass('jf-bg');
-            _this.append(html);
-
-
-            $(this).off('click');
-            $(this).on('click', 'a.json-toggle', function() {
-                var target = $(this).toggleClass('collapsed').siblings('ul.json-dict, ol.json-array');
-                target.toggle();
-                if (target.is(':visible')) {
-                    target.siblings('.json-placeholder').remove();
-                }
-                else {
-                    var count = target.children('li').length;
-                    var placeholder = count + (count > 1 ? ' items' : ' item');
-                    target.after('<a href class="json-placeholder">' + placeholder + '</a>');
-                }
-                return false;
-            });
-
-            /* Simulate click on toggle button when placeholder is clicked */
-            $(this).on('click', 'a.json-placeholder', function() {
-                $(this).siblings('a.json-toggle').click();
-                return false;
-            });
-
-            if (defaultOptions.collapsed == true) {
-                /* Trigger click to collapse all nodes */
-                $(this).find('a.json-toggle').click();
-            }
-        });
-
-
-
-
-        /*_this.css('display', 'none');
-
+        jf.hide(_this);
         if (json == null) {
             try {
                 json = JSON.parse(_this.text());
             } catch(e) {
-                console.error('非json格式')
+                jf.show(_this);
+                return;
             }
         }
         if (json == null) {
@@ -164,8 +124,46 @@
             return;
         }
 
-        $('<pre>').appendTo(_this).html(JSON.stringify(json, undefined, '<li></li>'));
-        jf.show(_this);*/
+        if (typeof json === 'string') {
+            try {
+                json = JSON.parse(json);
+            } catch(e) {
+                jf.show(_this);
+                return;
+            }
+        }
+        _this.html('<a href class="json-root json-toggle">JSON</a>')
+
+        return this.each(function() {
+            var html = json2html(json, defaultOptions);
+            _this.css('font-size', defaultOptions.fontSize + 'px');
+            _this.css('line-height', defaultOptions.lineHeight + 'px');
+            _this.addClass('jf-main');
+            _this.append(html);
+            jf.show(_this);
+
+            $(this).off('click');
+            $(this).on('click', '.json-toggle-btn', function() {
+                var target = $(this).toggleClass('collapsed').siblings('ul.json-dict, ol.json-array');
+                target.toggle();
+                if (target.is(':visible')) {
+                    target.siblings('.json-placeholder').remove();
+                } else {
+                    var count = target.children('li').length;
+                    var placeholder = count + (count > 1 ? ' items' : ' item');
+                    target.after('<span class="json-placeholder">' + placeholder + '</span>');
+                }
+                return false;
+            });
+
+            $(this).on('click', '.json-placeholder', function() {
+                $(this).siblings('.json-toggle-btn').click();
+                return false;
+            });
+            if (defaultOptions.collapsed == true) {
+                $(this).find('.json-toggle-btn').click();
+            }
+        });
 
     };
 })(jQuery);
